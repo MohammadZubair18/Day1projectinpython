58da0447e66822dd04359c476613fb3d376bff3e
MohammadZubair18 <116421874+MohammadZubair18@users.noreply.github.com>
2022-10-22 11:43:10 +0530

    Create codefileofday1pythonproject.py
    Python String Concatenation
    String concatenation means add strings together.
    Use the + character to add a variable to another variable
    Example
    x = "Python is "
    y = "awesome"
    z =  x + y
    print(z)

---
+++ codefileofday1pythonproject.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Welcome to Brand New Project")
+#2. Ask the user for the city that they grew up in.
+city= input("What is Your City Name\n")
+#3. Ask the user for the name of a pet.
+item= input("What is Your Delicious Food in your City\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your area is Famous for " + city  + "" + item)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
